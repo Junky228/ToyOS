6c51642cdfb13dfb5a3758e56e37a213dc940ceb
Hebert Julian <julianhebert@comcast.net>
2012-05-03 12:14:06 -0400

    section 2 done

--- write.c
+++ write.c
@@ -4,19 +4,28 @@
 
 int main()
 {
-	char boot_buf[512];
-	int floppy_desc, file_desc;
+		char boot_buf[512];
+		int floppy_desc, file_desc;
 
+		file_desc = open("./bsect", O_RDONLY);
 
-	file_desc = open("./boot", O_RDONLY);
-	read(file_desc, boot_buf, 510);
-	close(file_desc);
+		read(file_desc, boot_buf, 510);
+		close(file_desc);
 
-	boot_buf[510] = 0x55;
-	boot_buf[511] = 0xaa;
+		boot_buf[510] = 0x55;
+		boot_buf[511] = 0xaa;
 
-	floppy_desc = open("/dev/fd0", O_RDWR);
-	lseek(floppy_desc, 0, SEEK_CUR);
-	write(floppy_desc, boot_buf, 512);
-	close(floppy_desc);
-} 
+		floppy_desc = open("/dev/fd0", O_RDWR);
+
+		lseek(floppy_desc, 0, SEEK_SET);
+		write(floppy_desc, boot_buf, 512);
+
+		file_desc = open("./sect2", O_RDONLY);
+		read(file_desc, boot_buf, 512);
+		close(file_desc);
+
+		lseek(floppy_desc, 512, SEEK_SET);
+		write(floppy_desc, boot_buf, 512);
+
+		close(floppy_desc);
+}
